655baa6e01b7aec08ad10b434c8ded34c8a161e2
IrfanAli17899 <irfanali.17899@gmail.com>
2018-09-16 23:42:58 +0500

    created delete route for a todo from frontend you have to give /_id for update and delete or to find a particular todo

--- Part1 - Server API's/Step-1/server/routes/todo.ts
+++ Part1 - Server API's/Step-1/server/routes/todo.ts
@@ -1,47 +1,56 @@
-import {Router,Response,Request} from "express";
+import { Router, Response, Request } from "express";
 import Todo from "../models/todo";
 
 
 class TodoRoute {
-    router:Router;
-    constructor(){
+    router: Router;
+    constructor() {
         this.router = Router();
         this.routes();
     }
 
-    getAllTodo(req:Request,res:Response){
-        Todo.find().then((result)=>{
+    getAllTodo(req: Request, res: Response) {
+        Todo.find().then((result) => {
             res.status(200).send(result)
-        }).catch((err)=>{
+        }).catch((err) => {
             res.status(400).send(err)
         })
     }
 
-    PostTodo(req:Request,res:Response){
-        let { title , description } = req.body
-        var todo = new Todo({title,description})
-        todo.save().then((result)=>{
+    PostTodo(req: Request, res: Response) {
+        let { title, description } = req.body
+        var todo = new Todo({ title, description })
+        todo.save().then((result) => {
             res.status(201).send(result)
-        }).catch((err)=>{
+        }).catch((err) => {
             res.status(400).send(err)
         })
     }
 
-        getTodo(req:Request,res:Response){
-            var {_id } = req.params;
-            Todo.findById(_id).then((result)=>{
-                res.status(200).send(result);
-            }).catch((err)=>{
-                res.status(400).send(err);
-            })
-        }
+    getTodo(req: Request, res: Response) {
+        var { _id } = req.params;
+        Todo.findById(_id).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            res.status(400).send(err);
+        })
+    }
 
-       
+    deleteTodo(req: Request, res: Response) {
+        var { _id } = req.params;
+        Todo.findByIdAndDelete(_id).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            res.status(400).send(err);
+        })
+    }
 
-    routes(){
-         this.router.get("/",this.getAllTodo)
-         this.router.post("/",this.PostTodo)
-         this.router.get("/:_id",this.getTodo)
+    routes() {
+        this.router.get("/", this.getAllTodo);
+        this.router.post("/", this.PostTodo);
+        this.router.get("/:_id", this.getTodo);
+        this.router.delete("/:_id",this.deleteTodo);
+
     }
 }
 
