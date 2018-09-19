9662a4d29e86a7a1303c62972efc1e2b619b914a
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2018-09-19 20:35:18 +0500

    define routes i.e GET,POST,PUT,DELETE

---
+++ Part1/Step - 2/src/controllers/todo.ts
@@ -0,0 +1,78 @@
+import { Router } from 'express';
+import Todo from '../models/todo';
+export const todo = Router();
+
+todo.get('/tasks', async (req, res, next) => {
+  Todo.findAll()
+    .then(function (result) {
+      if (result.length === 0) {
+        res.status(404).send("Not Found")
+      }
+      return res.send(result)
+    })
+    .catch(function (err) {
+      return res.send(err)
+    })
+});
+
+todo.get('/tasks/:id', async (req, res, next) => {
+  Todo.findById(req.params.id)
+    .then(function (result) {
+      if (!result) {
+        return res.send("Not Found")
+      }
+      return res.send(result)
+    })
+    .catch(function (err) {
+      return res.send(err)
+    });
+});
+
+todo.post('/tasks', async (req, res, next) => {
+  const todo = new Todo({
+    title: req.body.title,
+    description: req.body.description,
+    done: req.body.done
+  });
+  todo.save()
+    .then(function (result) {
+      return res.send(result)
+    })
+    .catch(function (err) {
+      return res.send(err)
+    });
+});
+
+todo.put('/tasks/:id', async (req, res, next) => {
+  Todo.update({
+    title: req.body.title,
+    description: req.body.description,
+    done: req.body.done
+  },
+    { returning: true, where: { id: req.params.id } }
+  )
+    .then(function (result) {
+      return res.send(result)
+    })
+    .catch(function (err) {
+      return res.send(err)
+    });
+
+});
+
+todo.delete('/tasks/:id', async (req, res, next) => {
+  Todo.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(function (result) {
+      if (!result) {
+        return res.send("Record doesnot exist");
+      }
+      return res.send("Successfully deleted")
+    })
+    .catch(function (err) {
+      return res.send(err)
+    });
+});
