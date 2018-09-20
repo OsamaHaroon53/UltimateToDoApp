bf631e067427064da1a0d7acd8576a80c05165f7
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2018-09-21 01:44:30 +0500

    add methods for get,post,put and delete Todo

--- Part 2/Step5/src/app/services/todo.service.ts
+++ Part 2/Step5/src/app/services/todo.service.ts
@@ -7,4 +7,38 @@ import { map } from "rxjs/operators";
 export class TodoService {
 
   constructor(private http: HttpClient) { }
+
+  getList() {
+    return this.http.get("http://localhost:3000/todo/api/v1.0/tasks");
+  }
+
+  adList(todo) {
+    return this.http.post("http://localhost:3000/todo/api/v1.0/tasks", todo)
+      .pipe(map(res => {
+        if (res) {
+          return true;
+        }
+        return false;
+      }));
+  }
+
+  updateList(todo, id) {
+    return this.http.put("http://localhost:3000/todo/api/v1.0/tasks/" + id, todo)
+      .pipe(map(res => {
+        if (res) {
+          return true;
+        }
+        return false;
+      }));
+  }
+
+  delList(id) {
+    return this.http.delete("http://localhost:3000/todo/api/v1.0/tasks/" + id)
+      .pipe(map(res => {
+        if (res) {
+          return true;
+        }
+        return false;
+      }));
+  }
 }
