fcf5d77b66d1df8d6c72c174bbfdc89c01ea3626
IrfanAli17899 <irfanali.17899@gmail.com>
2018-09-21 13:27:05 +0500

    Firebase Initialized

--- Part3/src/App.js
+++ Part3/src/App.js
@@ -3,17 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data:null
+    }
+  }
+  componentDidMount(){
+    // const database = firebase.database();
+    // database.ref("basic")
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1>Hello World</h1>
     );
   }
 }
--- Part3/src/index.js
+++ Part3/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from "firebase";
+  var config = {
+    apiKey: "AIzaSyDt0Z_ldFeIS6nBPZ23HZfsC1YgjqOmh84",
+    authDomain: "react-todo-app-17899.firebaseapp.com",
+    databaseURL: "https://react-todo-app-17899.firebaseio.com",
+    projectId: "react-todo-app-17899",
+    storageBucket: "react-todo-app-17899.appspot.com",
+    messagingSenderId: "671605862627"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+if(module.hot){
+    module.hot.accept();
+}
 registerServiceWorker();
