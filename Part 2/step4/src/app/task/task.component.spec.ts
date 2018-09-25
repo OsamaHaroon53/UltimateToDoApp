94c030ea30bb8751d25faeb959cafb26c586d86e
OsamaHaroon53 <42175607+OsamaHaroon53@users.noreply.github.com>
2018-09-25 17:46:12 +0500

    task test updated

--- Part 2/step4/src/app/task/task.component.spec.ts
+++ Part 2/step4/src/app/task/task.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskComponent } from './task.component';
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from '@angular/http';
+import { By } from '@angular/platform-browser';
 
 describe('TaskComponent', () => {
     let component: TaskComponent;
@@ -22,9 +23,17 @@ describe('TaskComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(TaskComponent);
         component = fixture.componentInstance;
+        component.todolist = [{ _id: '123jakdkkdkdd', title: "hello", description: "world", done: false }]
         fixture.detectChanges();
     });
 
+    it('should render all todos', () => {
+    let de = fixture.debugElement.queryAll(By.css('td'));
+    expect(de[0].nativeElement.innerHTML).toBe('123jakdkkdkdd');
+    expect(de[1].nativeElement.innerHTML).toBe('hello');
+    expect(de[2].nativeElement.innerHTML).toBe('world');
+    expect(de[3].nativeElement.innerHTML).toBe('false');
+  });
     it('should create', () => {
         expect(component).toBeTruthy();
     });
