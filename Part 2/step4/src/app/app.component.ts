7fe18f8a9f64ce2e234f8ae71a9a76e1e984da6d
OsamaHaroon53 <42175607+OsamaHaroon53@users.noreply.github.com>
2018-09-25 17:56:06 +0500

    add SWUpdate package

--- Part 2/step4/src/app/app.component.ts
+++ Part 2/step4/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-// import { SwUpdate } from '@angular/service-worker';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'To Do';
-  constructor() { }
+  constructor(private sw: SwUpdate) {
+      sw.available.subscribe(ev => {
+      sw.activateUpdate().then(() => document.location.reload())
+    });
+  }
+    ngOnInit() {
+      this.sw.available.subscribe((ev) => {
+      window.location.reload();
+    });
+  }
 }
